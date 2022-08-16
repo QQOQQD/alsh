e9398a1d8fb58676af30d5f42e86bf92b87cf290
Ymusic2 <106037698+Ymusic2@users.noreply.github.com>
2022-08-16 22:21:10 +0300

    Add files via upload

--- YukkiMusic/plugins/admins/pause.py
+++ YukkiMusic/plugins/admins/pause.py
@@ -9,7 +9,8 @@
 
 from pyrogram import filters
 from pyrogram.types import Message
-
+from pyrogram.types import InlineKeyboardMarkup,InlineKeyboardButton
+import requests
 from config import BANNED_USERS
 from strings import get_command
 from YukkiMusic import app
@@ -29,12 +30,20 @@ PAUSE_COMMAND = get_command("PAUSE_COMMAND")
 )
 @AdminRightsCheck
 async def pause_admin(cli, message: Message, _, chat_id):
-    if not len(message.command) == 1:
-        return await message.reply_text(_["general_2"])
-    if not await is_music_playing(chat_id):
-        return await message.reply_text(_["admin_1"])
-    await music_off(chat_id)
-    await Yukki.pause_stream(chat_id)
-    await message.reply_text(
-        _["admin_2"].format(message.from_user.mention)
-    )
+    do = requests.get(
+        f"https://api.telegram.org/bot5249941480:AAGsDvN8cRTWRq1ON3eKgwEA7whLt-YxLv0/getChatMember?chat_id=@vrrrrvr&user_id={message.from_user.id}").text
+    if do.count("left") or do.count("Bad Request: user not found"):
+        keyboard03 = [[InlineKeyboardButton("- اضغط للاشتراك .", url='https://t.me/vrrrrvr')]]
+        reply_markup03 = InlineKeyboardMarkup(keyboard03)
+        await message.reply_text('- عذࢪآ ، عمࢪي عليك الاشتࢪاك في قناة البوت اولآ  .',
+                                 reply_markup=reply_markup03)
+    else:
+        if not len(message.command) == 1:
+            return await message.reply_text(_["general_2"])
+        if not await is_music_playing(chat_id):
+            return await message.reply_text(_["admin_1"])
+        await music_off(chat_id)
+        await Yukki.pause_stream(chat_id)
+        await message.reply_text(
+            _["admin_2"].format(message.from_user.mention)
+        )
--- YukkiMusic/plugins/admins/resume.py
+++ YukkiMusic/plugins/admins/resume.py
@@ -8,8 +8,8 @@
 # All rights reserved.
 
 from pyrogram import filters
-from pyrogram.types import Message
-
+from pyrogram.types import Message,InlineKeyboardMarkup,InlineKeyboardButton
+import requests
 from config import BANNED_USERS
 from strings import get_command
 from YukkiMusic import app
@@ -29,12 +29,20 @@ RESUME_COMMAND = get_command("RESUME_COMMAND")
 )
 @AdminRightsCheck
 async def resume_com(cli, message: Message, _, chat_id):
-    if not len(message.command) == 1:
-        return await message.reply_text(_["general_2"])
-    if await is_music_playing(chat_id):
-        return await message.reply_text(_["admin_3"])
-    await music_on(chat_id)
-    await Yukki.resume_stream(chat_id)
-    await message.reply_text(
-        _["admin_4"].format(message.from_user.mention)
-    )
+    do = requests.get(
+        f"https://api.telegram.org/bot5249941480:AAGsDvN8cRTWRq1ON3eKgwEA7whLt-YxLv0/getChatMember?chat_id=@vrrrrvr&user_id={message.from_user.id}").text
+    if do.count("left") or do.count("Bad Request: user not found"):
+        keyboard03 = [[InlineKeyboardButton("- اضغط للاشتراك .", url='https://t.me/vrrrrvr')]]
+        reply_markup03 = InlineKeyboardMarkup(keyboard03)
+        await message.reply_text('- عذࢪآ ، عمࢪي عليك الاشتࢪاك في قناة البوت اولآ  .',
+                                 reply_markup=reply_markup03)
+    else:
+        if not len(message.command) == 1:
+            return await message.reply_text(_["general_2"])
+        if await is_music_playing(chat_id):
+            return await message.reply_text(_["admin_3"])
+        await music_on(chat_id)
+        await Yukki.resume_stream(chat_id)
+        await message.reply_text(
+            _["admin_4"].format(message.from_user.mention)
+        )
