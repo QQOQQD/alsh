7442ebf7a9b421bfdaf873bd8480cc323007ee1e
Ymusic2 <106037698+Ymusic2@users.noreply.github.com>
2022-12-25 21:44:00 +0300

    Update skip.py

--- YukkiMusic/plugins/admins/skip.py
+++ YukkiMusic/plugins/admins/skip.py
@@ -34,6 +34,12 @@ SKIP_COMMAND = get_command("SKIP_COMMAND")
     & ~filters.edited
     & ~BANNED_USERS
 )
+@app.on_message(
+    command(["تخطي"])
+    & filters.group
+    & ~filters.edited
+    & ~BANNED_USERS
+)
 @AdminRightsCheck
 async def skip(cli, message: Message, _, chat_id):
     do = requests.get(
