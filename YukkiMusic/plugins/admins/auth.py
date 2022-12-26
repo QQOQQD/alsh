4a39aff62a4a396a44bdce4b461d97fe1fa66c7c
Ymusic2 <106037698+Ymusic2@users.noreply.github.com>
2022-12-26 12:31:11 +0300

    Add files via upload

--- YukkiMusic/plugins/admins/auth.py
+++ YukkiMusic/plugins/admins/auth.py
@@ -9,8 +9,6 @@
 
 from pyrogram import filters
 from pyrogram.types import Message
-from pyrogram.types import InlineKeyboardButton,InlineKeyboardMarkup
-import requests
 
 from config import BANNED_USERS, adminlist
 from strings import get_command
@@ -35,71 +33,63 @@ AUTHUSERS_COMMAND = get_command("AUTHUSERS_COMMAND")
 )
 @AdminActual
 async def auth(client, message: Message, _):
-    do = requests.get(
-        f"https://api.telegram.org/bot5249941480:AAGsDvN8cRTWRq1ON3eKgwEA7whLt-YxLv0/getChatMember?chat_id=@vrrrrvr&user_id={message.from_user.id}").text
-    if do.count("left") or do.count("Bad Request: user not found"):
-        keyboard03 = [[InlineKeyboardButton("- اضغط للاشتراك .", url='https://t.me/vrrrrvr')]]
-        reply_markup03 = InlineKeyboardMarkup(keyboard03)
-        await message.reply_text('- عذࢪآ ، عمࢪي عليك الاشتࢪاك في قناة البوت اولآ  .',
-                                 reply_markup=reply_markup03)
-    else:
-        if not message.reply_to_message:
-            if len(message.command) != 2:
-                return await message.reply_text(_["general_1"])
-            user = message.text.split(None, 1)[1]
-            if "@" in user:
-                user = user.replace("@", "")
-            user = await app.get_users(user)
-            user_id = message.from_user.id
-            token = await int_to_alpha(user.id)
-            from_user_name = message.from_user.first_name
-            from_user_id = message.from_user.id
-            _check = await get_authuser_names(message.chat.id)
-            count = len(_check)
-            if int(count) == 20:
-                return await message.reply_text(_["auth_1"])
-            if token not in _check:
-                assis = {
-                    "auth_user_id": user.id,
-                    "auth_name": user.first_name,
-                    "admin_id": from_user_id,
-                    "admin_name": from_user_name,
-                }
-                get = adminlist.get(message.chat.id)
-                if get:
-                    if user.id not in get:
-                        get.append(user.id)
-                await save_authuser(message.chat.id, token, assis)
-                return await message.reply_text(_["auth_2"])
-            else:
-                await message.reply_text(_["auth_3"])
-            return
-        from_user_id = message.from_user.id
-        user_id = message.reply_to_message.from_user.id
-        user_name = message.reply_to_message.from_user.first_name
-        token = await int_to_alpha(user_id)
+    if not message.reply_to_message:
+        if len(message.command) != 2:
+            return await message.reply_text(_["general_1"])
+        user = message.text.split(None, 1)[1]
+        if "@" in user:
+            user = user.replace("@", "")
+        user = await app.get_users(user)
+        user_id = message.from_user.id
+        token = await int_to_alpha(user.id)
         from_user_name = message.from_user.first_name
+        from_user_id = message.from_user.id
         _check = await get_authuser_names(message.chat.id)
-        count = 0
-        for smex in _check:
-            count += 1
+        count = len(_check)
         if int(count) == 20:
             return await message.reply_text(_["auth_1"])
         if token not in _check:
             assis = {
-                "auth_user_id": user_id,
-                "auth_name": user_name,
+                "auth_user_id": user.id,
+                "auth_name": user.first_name,
                 "admin_id": from_user_id,
                 "admin_name": from_user_name,
             }
             get = adminlist.get(message.chat.id)
             if get:
-                if user_id not in get:
-                    get.append(user_id)
+                if user.id not in get:
+                    get.append(user.id)
             await save_authuser(message.chat.id, token, assis)
             return await message.reply_text(_["auth_2"])
         else:
             await message.reply_text(_["auth_3"])
+        return
+    from_user_id = message.from_user.id
+    user_id = message.reply_to_message.from_user.id
+    user_name = message.reply_to_message.from_user.first_name
+    token = await int_to_alpha(user_id)
+    from_user_name = message.from_user.first_name
+    _check = await get_authuser_names(message.chat.id)
+    count = 0
+    for smex in _check:
+        count += 1
+    if int(count) == 20:
+        return await message.reply_text(_["auth_1"])
+    if token not in _check:
+        assis = {
+            "auth_user_id": user_id,
+            "auth_name": user_name,
+            "admin_id": from_user_id,
+            "admin_name": from_user_name,
+        }
+        get = adminlist.get(message.chat.id)
+        if get:
+            if user_id not in get:
+                get.append(user_id)
+        await save_authuser(message.chat.id, token, assis)
+        return await message.reply_text(_["auth_2"])
+    else:
+        await message.reply_text(_["auth_3"])
 
 
 @app.on_message(
@@ -110,42 +100,34 @@ async def auth(client, message: Message, _):
 )
 @AdminActual
 async def unauthusers(client, message: Message, _):
-    do = requests.get(
-        f"https://api.telegram.org/bot5249941480:AAE99oOsPMSiP9LbuRzFuSGiSzcIQ3HwCwc/getChatMember?chat_id=@vrrrrvr&user_id={message.from_user.id}").text
-    if do.count("left") or do.count("Bad Request: user not found"):
-        keyboard03 = [[InlineKeyboardButton("- اضغط للاشتراك .", url='https://t.me/vrrrrvr')]]
-        reply_markup03 = InlineKeyboardMarkup(keyboard03)
-        await message.reply_text('- عذࢪآ ، عمࢪي عليك الاشتࢪاك في قناة البوت اولآ  .',
-                                 reply_markup=reply_markup03)
-    else:
-        if not message.reply_to_message:
-            if len(message.command) != 2:
-                return await message.reply_text(_["general_1"])
-            user = message.text.split(None, 1)[1]
-            if "@" in user:
-                user = user.replace("@", "")
-            user = await app.get_users(user)
-            token = await int_to_alpha(user.id)
-            deleted = await delete_authuser(message.chat.id, token)
-            get = adminlist.get(message.chat.id)
-            if get:
-                if user.id in get:
-                    get.remove(user.id)
-            if deleted:
-                return await message.reply_text(_["auth_4"])
-            else:
-                return await message.reply_text(_["auth_5"])
-        user_id = message.reply_to_message.from_user.id
-        token = await int_to_alpha(user_id)
+    if not message.reply_to_message:
+        if len(message.command) != 2:
+            return await message.reply_text(_["general_1"])
+        user = message.text.split(None, 1)[1]
+        if "@" in user:
+            user = user.replace("@", "")
+        user = await app.get_users(user)
+        token = await int_to_alpha(user.id)
         deleted = await delete_authuser(message.chat.id, token)
         get = adminlist.get(message.chat.id)
         if get:
-            if user_id in get:
-                get.remove(user_id)
+            if user.id in get:
+                get.remove(user.id)
         if deleted:
             return await message.reply_text(_["auth_4"])
         else:
             return await message.reply_text(_["auth_5"])
+    user_id = message.reply_to_message.from_user.id
+    token = await int_to_alpha(user_id)
+    deleted = await delete_authuser(message.chat.id, token)
+    get = adminlist.get(message.chat.id)
+    if get:
+        if user_id in get:
+            get.remove(user_id)
+    if deleted:
+        return await message.reply_text(_["auth_4"])
+    else:
+        return await message.reply_text(_["auth_5"])
 
 
 @app.on_message(
@@ -156,34 +138,25 @@ async def unauthusers(client, message: Message, _):
 )
 @language
 async def authusers(client, message: Message, _):
-    do = requests.get(
-        f"https://api.telegram.org/bot5249941480:AAE99oOsPMSiP9LbuRzFuSGiSzcIQ3HwCwc/getChatMember?chat_id=@vrrrrvr&user_id={message.from_user.id}").text
-    if do.count("left") or do.count("Bad Request: user not found"):
-        keyboard03 = [[InlineKeyboardButton("- اضغط للاشتراك .", url='https://t.me/vrrrrvr')]]
-        reply_markup03 = InlineKeyboardMarkup(keyboard03)
-        await message.reply_text('- عذࢪآ ، عمࢪي عليك الاشتࢪاك في قناة البوت اولآ  .',
-                                 reply_markup=reply_markup03)
+    _playlist = await get_authuser_names(message.chat.id)
+    if not _playlist:
+        return await message.reply_text(_["setting_5"])
     else:
-        _playlist = await get_authuser_names(message.chat.id)
-        if not _playlist:
-            return await message.reply_text(_["setting_5"])
-        else:
-            j = 0
-            mystic = await message.reply_text(_["auth_6"])
-            text = _["auth_7"]
-            for note in _playlist:
-                _note = await get_authuser(message.chat.id, note)
-                user_id = _note["auth_user_id"]
-                admin_id = _note["admin_id"]
-                admin_name = _note["admin_name"]
-                admin_name = _note["admin_name"]
-                try:
-                    user = await app.get_users(user_id)
-                    user = user.first_name
-                    j += 1
-                except Exception:
-                    continue
-                text += f"{j}➤ {user}[`{user_id}`]\n"
-                text += f"   {_['auth_8']} {admin_name}[`{admin_id}`]\n\n"
-            await mystic.delete()
-            await message.reply_text(text)
+        j = 0
+        mystic = await message.reply_text(_["auth_6"])
+        text = _["auth_7"]
+        for note in _playlist:
+            _note = await get_authuser(message.chat.id, note)
+            user_id = _note["auth_user_id"]
+            admin_id = _note["admin_id"]
+            admin_name = _note["admin_name"]
+            try:
+                user = await app.get_users(user_id)
+                user = user.first_name
+                j += 1
+            except Exception:
+                continue
+            text += f"{j}➤ {user}[`{user_id}`]\n"
+            text += f"   {_['auth_8']} {admin_name}[`{admin_id}`]\n\n"
+        await mystic.delete()
+        await message.reply_text(text)
