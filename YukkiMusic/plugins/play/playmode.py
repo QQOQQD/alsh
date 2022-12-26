3c1cacd79f8885e9c20586c4fe038dbba406b6a8
Ymusic2 <106037698+Ymusic2@users.noreply.github.com>
2022-12-26 12:36:37 +0300

    Add files via upload

--- YukkiMusic/plugins/play/playmode.py
+++ YukkiMusic/plugins/play/playmode.py
@@ -8,8 +8,7 @@
 # All rights reserved.
 
 from pyrogram import filters
-from pyrogram.types import InlineKeyboardMarkup, Message,InlineKeyboardButton
-import requests
+from pyrogram.types import InlineKeyboardMarkup, Message
 
 from config import BANNED_USERS
 from strings import get_command
@@ -31,31 +30,23 @@ PLAYMODE_COMMAND = get_command("PLAYMODE_COMMAND")
 )
 @language
 async def playmode_(client, message: Message, _):
-    do = requests.get(
-        f"https://api.telegram.org/bot5249941480:AAGsDvN8cRTWRq1ON3eKgwEA7whLt-YxLv0/getChatMember?chat_id=@vrrrrvr&user_id={message.from_user.id}").text
-    if do.count("left") or do.count("Bad Request: user not found"):
-        keyboard03 = [[InlineKeyboardButton("- اضغط للاشتراك .", url='https://t.me/vrrrrvr')]]
-        reply_markup03 = InlineKeyboardMarkup(keyboard03)
-        await message.reply_text('- عذࢪآ ، عمࢪي عليك الاشتࢪاك في قناة البوت اولآ  .',
-                                 reply_markup=reply_markup03)
+    playmode = await get_playmode(message.chat.id)
+    if playmode == "Direct":
+        Direct = True
     else:
-        playmode = await get_playmode(message.chat.id)
-        if playmode == "Direct":
-            Direct = True
-        else:
-            Direct = None
-        is_non_admin = await is_nonadmin_chat(message.chat.id)
-        if not is_non_admin:
-            Group = True
-        else:
-            Group = None
-        playty = await get_playtype(message.chat.id)
-        if playty == "Everyone":
-            Playtype = None
-        else:
-            Playtype = True
-        buttons = playmode_users_markup(_, Direct, Group, Playtype)
-        response = await message.reply_text(
-            _["playmode_1"].format(message.chat.title),
-            reply_markup=InlineKeyboardMarkup(buttons),
-        )
+        Direct = None
+    is_non_admin = await is_nonadmin_chat(message.chat.id)
+    if not is_non_admin:
+        Group = True
+    else:
+        Group = None
+    playty = await get_playtype(message.chat.id)
+    if playty == "Everyone":
+        Playtype = None
+    else:
+        Playtype = True
+    buttons = playmode_users_markup(_, Direct, Group, Playtype)
+    response = await message.reply_text(
+        _["playmode_1"].format(message.chat.title),
+        reply_markup=InlineKeyboardMarkup(buttons),
+    )
